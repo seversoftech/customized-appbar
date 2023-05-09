938beee2262ea1dbdbaf2366c74f5ff312d22665
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-09 05:23:32 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -18,4 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
